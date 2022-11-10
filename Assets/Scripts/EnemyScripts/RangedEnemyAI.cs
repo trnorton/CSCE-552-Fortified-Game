08b0472eb7fac63372eb7729d0956b470209f864
mevinson <mevinson@email.sc.edu>
2022-11-10 17:36:40 -0500

    issue with ranged enemy

--- Assets/Scripts/EnemyScripts/RangedEnemyAI.cs
+++ Assets/Scripts/EnemyScripts/RangedEnemyAI.cs
@@ -192,9 +192,8 @@ public class RangedEnemyAI : MonoBehaviour
             isFiring = true;
             GameObject newProjectile = Instantiate(projectile, firePoint.position, firePoint.rotation);
             newProjectile.GetComponent<Rigidbody>().AddForce(newProjectile.transform.forward * fireSpeed);
-            isFiring = false;
             yield return new WaitForSeconds(fireTimer);
-
+            isFiring = false;
     }
 
     public GameObject getClosestWall()
