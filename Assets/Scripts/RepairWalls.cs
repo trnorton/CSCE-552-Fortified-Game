c935fde85b0581b9cdb6a529bc13119134486831
Taylor Norton <trnorton@email.sc.edu>
2022-11-15 15:37:48 -0500

    Fixed repair wall bug

--- Assets/Scripts/RepairWalls.cs
+++ Assets/Scripts/RepairWalls.cs
@@ -16,14 +16,14 @@ public class RepairWalls : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
-        
+
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
     }
 
     public void onButtonPress()
@@ -34,11 +34,11 @@ public class RepairWalls : MonoBehaviour
         //Getting Player cash
         var playercash = player.GetComponent<Money>();
         money = playercash.MoneyToInt();
-        
+
         //if the walls are wooden
         if(wallLevel == 0)
         {
-            if(money >= repairCost)
+            if(money >= repairCost && GameObject.FindGameObjectsWithTag("destroyedWall").Length != 0)
             {
                 playercash.SubMoney(repairCost);
                 repairWoodWalls();
@@ -49,7 +49,7 @@ public class RepairWalls : MonoBehaviour
                 Debug.Log("Not enough funds");
             }
         }
-        if(wallLevel == 1)
+        if(wallLevel == 1 && GameObject.FindGameObjectsWithTag("destroyedWall").Length != 0)
         {
             if(money >= repairCost)
             {
@@ -87,7 +87,7 @@ public class RepairWalls : MonoBehaviour
         {
             Instantiate(stoneWallPrefab, walls[i].transform.position, walls[i].transform.rotation);
             Destroy(walls[i]);
-            
+
         }
         for(int j = 0; j < destroyedWalls.Length; j++)
         {
