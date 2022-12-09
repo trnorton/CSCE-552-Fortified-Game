ab55e3084e5de23808f393053a3a7c9deb7072fa
Brandon Carter <bcarter9893@gmail.com>
2022-12-09 01:17:16 -0500

    Comp Error

--- Assets/Scripts/EnemyScripts/GhostAI.cs
+++ Assets/Scripts/EnemyScripts/GhostAI.cs
@@ -80,7 +80,7 @@ public class GhostAI : MonoBehaviour
     //Enemy goes after player
     private void ChasePlayer()
     {
-        agent,isStopped = false;
+        agent.isStopped = false;
         agent.SetDestination(player.transform.position);
     }
     //Atack Treasure
