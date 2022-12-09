5647941eb9165f6269ca490d8cf6450941b5eb09
Taylor Norton <trnorton@email.sc.edu>
2022-12-09 00:22:41 -0500

    Revert "switched boss tag to enemy"
    This reverts commit 86112bf2a288a55f25e6be223182cb422c6b81e3.

--- Assets/Scripts/EnemyScripts/MeleeEnemyAI.cs
+++ Assets/Scripts/EnemyScripts/MeleeEnemyAI.cs
@@ -52,7 +52,7 @@ public class MeleeEnemyAI : MonoBehaviour
         treasureDes = false;
         ObjectAnimator = this.GetComponent<Animator>();
         beingAttacked = false;
-        if(gameObject.name == "Zombie_boss(Clone)") {
+        if(gameObject.tag == "Boss") {
           Collider thisCollider = GetComponent<Collider>();
           GameObject eastWalkway = GameObject.FindWithTag("East Walkway");
           GameObject westWalkway = GameObject.FindWithTag("West Walkway");
